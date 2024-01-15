6022fff939ddf5166afa477e77561fcfe6cbd1c5
GrigoryEbl <123863131+GrigoryEbl@users.noreply.github.com>
2024-01-15 16:19:30 +0400

    11

--- Assets/Scripts/MoovingGeometry/DOTween/TextChanger.cs
+++ Assets/Scripts/MoovingGeometry/DOTween/TextChanger.cs
@@ -1,20 +1,28 @@
 using UnityEngine;
 using DG.Tweening;
 using UnityEngine.UI;
+using System.Collections;
 
 public class TextChanger : MonoBehaviour
 {
     [SerializeField] private Text _changedText;
-    [SerializeField] private Text _addedText;
-    [SerializeField] private Text _hackedText;
     [SerializeField] private float _duration;
 
     private void Start()
     {
+        StartCoroutine(Change());
+    }
+
+    private IEnumerator Change()
+    {
         _changedText.DOText("Changed text", _duration);
-        _addedText.DOText("Added text", _duration).SetRelative();
-        _hackedText.DOText("Hack text", _duration, true, ScrambleMode.All);
-        
+        yield return new WaitForSeconds(_duration);
+
+        _changedText.DOText("Added text", _duration).SetRelative();
+        yield return new WaitForSeconds(_duration);
+
+        _changedText.DOText("Hack text", _duration, true, ScrambleMode.All);
+        yield return new WaitForSeconds(_duration);
     }
 }
 
